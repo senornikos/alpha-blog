c2f994fa5861f1d18fd52bd724c324f8352a21c7
Nick Wakefield <senornikos@gmail.com>
2016-10-21 18:48:01 +0800

    bug in migrate

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20161021064048) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.text     "desciption"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
