d21015f97430ae679dc09e2fcfd081741820b5c1
Nick Wakefield <senornikos@gmail.com>
2016-10-21 22:42:28 +0800

    Updated flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
   def create
       @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was succesfully created"
+      flash[:success] = "Article was succesfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
 
   def update 
       if @article.update(article_params)
-        flash[:notice] = "Article as succesfully updated"
+        flash[:success] = "Article as succesfully updated"
         redirect_to article_path(@article)
       else
       render 'edit'        
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article was succesfully deleted"
+    flash[:danger] = "Article was succesfully deleted"
     redirect_to articles_path
   end
 
