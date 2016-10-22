209d59bd139687a7d93f5b80b806e31d5fb8b647
Nick Wakefield <senornikos@gmail.com>
2016-10-22 18:34:33 +0800

    create user tables and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021064048) do
+ActiveRecord::Schema.define(version: 20161022084445) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20161021064048) do
     t.text     "desciption"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
