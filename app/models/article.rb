42fb9aeb4bfaefda54651d4d9d90e51c6fa894d0
Nick Wakefield <senornikos@gmail.com>
2016-10-23 22:02:11 +0800

    Assocations with users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ActiveRecord::Base
+  
+  belongs_to :user
+
   validates :title, presence: true, length: {minimum: 3, maximum: 100}
   validates :description, presence: true, length: {minimum: 10,}
+  validates :user_id, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022084445) do
+ActiveRecord::Schema.define(version: 20161023134159) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20161022084445) do
     t.text     "desciption"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
