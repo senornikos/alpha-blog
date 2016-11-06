81fe02c39ccb1da36134554cc582b4fc20f336bc
Nick Wakefield <senornikos@gmail.com>
2016-11-06 18:36:56 +0800

    Added users and gravatar page details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = { size: 80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravator_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravator_url, alt: user.username, class: "img-circle") 
+  end
 end
