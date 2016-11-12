e37e806549b9f7970d8b0ba2c98e4a854a4be47c
Nick Wakefield <senornikos@gmail.com>
2016-11-12 17:57:59 +0800

    Add Admim user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106080252) do
+ActiveRecord::Schema.define(version: 20161112092557) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20161106080252) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
